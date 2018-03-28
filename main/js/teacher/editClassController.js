220ed114b82ccec138ff49b759c406c7f17de759
sunkink29 <calebparks29@gmail.com>
2018-03-28 11:19:04 -0500

    Fixed class size change bug

--- main/js/teacher/editClassController.js
+++ main/js/teacher/editClassController.js
@@ -14,23 +14,16 @@ app.controller('editClassController', function($scope, $rootScope, $timeout, $md
   controller.showBlockInfo = function(message) {
     controller.currentBlockInfo[0] = message[0];
     controller.currentBlockInfo[1] = message[1];
-    controller.checkAddButton();
     $scope.$apply();
   };
   
   controller.submitEdit = function() {
     postMethod("/teacher/setblocks", controller.currentBlockInfo);
-    controller.checkAddButton();
     controller.closeDialog();
   };
   
   controller.closeDialog = function() {
     $mdDialog.hide();
-  };
-  
-  controller.checkAddButton = function() {
-    controller.addButton0 = controller.currentBlockInfo[0].MaxSize <= $rootScope.mainView.NextStudents[0].length;
-   	controller.addButton1 = controller.currentBlockInfo[1].MaxSize <= $rootScope.mainView.NextStudents[1].length;
   };
   
   $(window).on('hashchange', function() {
